661b85539ea468bd29f9f052d0c5d4e8e962feff
Thatohatsi <marciatsoho@gmail.com>
2023-05-01 00:02:19 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - main function
  *
- * @n: character to compare
+ * @n: character to compare or the number of which the sign will be printed
  *
  * Return: 1 if positive, 0 if zero, -1 if negative
  *
@@ -15,11 +15,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
+	else
+	{
 	_putchar('-');
 	return (-1);
+	}
 }
