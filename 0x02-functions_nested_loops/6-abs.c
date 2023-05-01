19be97fd79b2d57bb775efa9a3dbdefa86dd5d30
Thatohatsi <marciatsoho@gmail.com>
2023-05-01 00:07:38 -0700

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,15 +2,15 @@
 /**
  * _abs - main function
  *
- * @n: integer to make absolute
- * Return: int or int times negative
+ * @n: integer to make absolute or the integer to be computed
+ * Return:the  absolute value of the integer
  *
  */
 
 int _abs(int n)
 {
-	if (n > 0)
+	if (n >= 0)
 		return (n);
 	else
-		return (n * -1);
+		return (-n);
 }
