3cf0434ec5bb8ab33fc8a63aeec5a4efd419321f
Thatohatsi <marciatsoho@gmail.com>
2023-05-02 19:42:59 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,9 +4,9 @@
  * _puts - Prints a string to stdout.
  * @str: The string to be printed.
  */
-void _puts(char *str);
+void _puts(char *str)
 {
-	while (*str);
+	while (*str)
 		_putchar(*str++);
 
 	_putchar('\n');
