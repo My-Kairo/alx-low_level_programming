4b5f27489d14e04eb8c65c40b928026228388f1b
Thatohatsi <marciatsoho@gmail.com>
2023-05-04 18:42:58 -0700

    first

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 /**
  * * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
+ *       used in the 0x06-pointers_arrays_strings directory.
  */
 
 int _putchar(char c);
